48aa6f96ee70444ab1dfe4ebf3466178a58cfeb8
Aleksander Dimitrov <alexander.dimitrov90@gmail.com>
2025-10-16 16:43:05 +0300

    added  react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Home from "./ui/Home.jsx";
+import Menu from "./features/menu/Menu.jsx";
 
+import CreateOrder from "./features/order/CreateOrder.jsx";
+import Order from "./features/order/Order.jsx";
+import Cart from "./features/cart/Cart.jsx";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Home />,
+	},
+	{
+		path: "/menu",
+		element: <Menu />,
+	},
+	{
+		path: "/cart",
+		element: <Cart />,
+	},
+	{
+		path: "/order/new",
+		element: <CreateOrder />,
+	},
+	{
+		path: "/order/:orderId",
+		element: <Order />,
+	},
+]);
 
 function App() {
-
-
-  return (
-    <>
-   hello
-    </>
-  )
+	return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
