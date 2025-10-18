8ae8a34d29c5a6698ae8d37635efcdc87555f02e
Aleksander Dimitrov <alexander.dimitrov90@gmail.com>
2025-10-18 19:24:31 +0300

    added styles for the loading comp

--- src/ui/LoadingComp.jsx
+++ src/ui/LoadingComp.jsx
@@ -1,4 +1,8 @@
 function LoadingComp() {
-	return <div className="loader"></div>;
+  return (
+    <div className="absolute inset-0 bg-slate-200/20 backdrop-blur-xs flex items-center justify-center">
+      <div className="loader"></div>;
+    </div>
+  );
 }
 export default LoadingComp;
