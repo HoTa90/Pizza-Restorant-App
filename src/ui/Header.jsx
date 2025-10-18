fe93e29ae5361c7f9bd9f11ba9875e875df0aede
Aleksander Dimitrov <alexander.dimitrov90@gmail.com>
2025-10-18 15:34:02 +0300

    added tailwind

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -3,7 +3,7 @@ import SearchOrder from "../features/order/SearchOrder.jsx";
 
 export default function Header() {
 	return (
-		<header>
+		<header className="bg-indigo-500">
 			<Link to={"/"}>Fast React Pizza Co.</Link>
 			<SearchOrder />
 			<p>TestUser</p>
