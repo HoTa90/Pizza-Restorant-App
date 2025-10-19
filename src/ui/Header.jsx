8e81802df3a6a230f39f2e9911d06ef5a63fb915
Aleksander Dimitrov <alexander.dimitrov90@gmail.com>
2025-10-19 14:47:18 +0300

    added new font

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,7 @@ import Username from "../features/user/Username.jsx";
 
 export default function Header() {
   return (
-    <header className="items-center flex justify-between border-b-2 border-stone-500 bg-indigo-400 px-4 py-3 font-semibold text-stone-100 uppercase sm:px-6">
+    <header className="items-center flex justify-between border-b-2 border-stone-500 bg-indigo-400 px-4 py-3 font-semibold text-stone-100 uppercase sm:px-6 ">
       <Link to={"/"} className="tracking-widest">
         Fast React Pizza Co.
       </Link>
