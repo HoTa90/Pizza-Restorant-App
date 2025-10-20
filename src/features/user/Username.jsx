2c4d1f8c53825c2a3b1246b99bcff5f60a321018
Aleksander Dimitrov <alexander.dimitrov90@gmail.com>
2025-10-20 14:20:29 +0300

    added user state

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,4 +1,14 @@
+import { useSelector } from "react-redux";
+
 function Username() {
-  return <div className="text-sm font-semibold hidden md:block">Test User</div>;
+  const username = useSelector((state) => state.user.username);
+
+	if (!username) {
+		return null
+	}
+
+  return (
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  );
 }
 export default Username;
--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,16 +1,20 @@
+import { useSelector } from "react-redux";
 import CreateUser from "../features/user/CreateUser.jsx";
+import Button from "./Button.jsx";
 
 function Home() {
+  const username = useSelector((state) => state.user.username);
+
   return (
-    <div className="my-10 sm:my-16 text-center">
-      <h1 className="mb-8 text-xl font-semibold text-stone-700 md:text-3xl px-4">
+    <div className="my-10 text-center sm:my-16">
+      <h1 className="mb-8 px-4 text-xl font-semibold text-stone-700 md:text-3xl">
         The best pizza.
         <br />
         <span className="text-indigo-600">
           Straight out of the oven, straight to you.
         </span>
       </h1>
-      <CreateUser />
+      {username=== "" ? <CreateUser /> : <Button to={"/menu"} type={"primary"}>To Menu</Button>}
     </div>
   );
 }
